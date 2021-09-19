c1e317f318562bf2249170e897efc6804170b391
ajitrajasekharan <ajitrajasekharan@icloud.com>
2021-09-19 20:34:55 +0000

    Minor fixes

--- extract_labels.sh
+++ extract_labels.sh
@@ -4,6 +4,8 @@ python label_stats.py adaptive_debug_pivots.txt
 echo "Stats after aggregation"
 python label_stats.py labels.txt
 cp stats_dict.txt results
+cat inferred.txt | sort -k3,3 -n -r  > tmp$$
+mv tmp$$ inferred.txt
 #cat labels.txt | awk '{if (NF != 5) print $0}'
 
 
